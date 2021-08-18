5b1041464357c525e4b9c48f1f0e17a5208b2fcf
uzzzzy <a.fauzy4b@gmail.com>
2021-08-18 11:35:39 +0800

    3

--- src/event.js
+++ src/event.js
@@ -1,12 +1,48 @@
-import React from "react";
+import { Component} from 'react'
 
-function Event() {
-  //Create function here
+function Event1() {
 
-  return (
-    // Code Inside div
-    <div></div>
-  );
+    function greetings(){
+        return alert('Hello morning everyone have a nice day')
+    }
+
+    return (
+        <div>
+            <p>If you press the Button alert Will show</p>
+            <button onClick={() => alert("Hello Full Stack Batch 25 participants")}>hello</button>
+
+            <p>If you press the Button alert Will show</p>
+            <button onClick={greetings}>Greeting</button>
+
+            
+            <p>If you press the Button alert Will show</p>
+            <input onChange={(e) => console.log(e.target.value)} />
+        </div>
+    )
 }
 
-export default Event;
+export default class Event extends Component {
+    constructor() {
+        super()
+    }
+
+    greetings(){
+        return alert('Hello morning everyone have a nice day')
+    }
+
+    render() {        
+        return (
+            <div>
+                <p>If you press the Button alert Will show</p>
+                <button onClick={() => alert("Hello Full Stack Batch 25 participants")}>hello</button>
+
+                <p>If you press the Button alert Will show</p>
+                <button onClick={this.greetings}>Greeting</button>
+
+                
+                <p>If you press the Button alert Will show</p>
+                <input onChange={(e) => console.log(e.target.value)} />
+            </div>
+        )
+    }
+}
